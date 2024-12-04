d0bed4f66da155e4376f395f97630de210ae3399
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2024-12-04 11:00:13 +0530

    add user page & table

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,13 +1,68 @@
-import { Breadcrumb } from 'antd';
+import { Breadcrumb, Space, Table } from 'antd';
 import { RightOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getUsers } from '../../http/api';
+import { User } from '../../types';
+
+const columns = [
+    {
+        title: 'ID',
+        dataIndex: 'id',
+        key: 'id',
+    },
+    {
+        title: 'Name',
+        dataIndex: 'firstname',
+        key: 'firstname',
+        render: (_text: string, record: User) => (
+            <Link to={`/users/${record.id}`}>
+                {record.firstname} {record.lastname}
+            </Link>
+        ),
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+    },
+    {
+        title: 'Role',
+        dataIndex: 'role',
+        key: 'role',
+    },
+];
+
 const Users = () => {
+    const {
+        data: users,
+        isLoading,
+        isError,
+        error,
+    } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await getUsers();
+            return res.data;
+        },
+    });
     return (
         <>
-            <Breadcrumb
-                separator={<RightOutlined />}
-                items={[{ title: <Link to="/">Dashboard</Link> }, { title: 'Users' }]}
-            />
+            <Space direction='vertical' size={'large'} style={{width: '100%'}}>
+                <Breadcrumb
+                    separator={<RightOutlined />}
+                    items={[
+                        { title: <Link to="/">Dashboard</Link> },
+                        { title: 'Users' },
+                    ]}
+                />
+
+                {isLoading && <div>Loading...</div>}
+
+                {isError && <div>{error.message}</div>}
+
+                <Table columns={columns} dataSource={users} />
+            </Space>
         </>
     );
 };
