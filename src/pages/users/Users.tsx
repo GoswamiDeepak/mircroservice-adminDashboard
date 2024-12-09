08db19a0b15d8c007456c19f27f1007b150b3708
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2024-12-09 17:23:25 +0530

    reset pagination on filter

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -129,7 +129,7 @@ const Users = () => {
 
     const debouncedQUpdate = useMemo(() => {
         return debounce((value: string | undefined) => {
-            setQueryParams((prev) => ({ ...prev, q: value }));
+            setQueryParams((prev) => ({ ...prev, q: value, currentPage: 1 }));
         }, 500);
     }, []);
 
@@ -143,7 +143,11 @@ const Users = () => {
         if ('q' in changedFilterFields) {
             debouncedQUpdate(changedFilterFields.q);
         } else {
-            setQueryParams((prev) => ({ ...prev, ...changedFilterFields }));
+            setQueryParams((prev) => ({
+                ...prev,
+                ...changedFilterFields,
+                currentPage: 1,
+            }));
         }
     };
 
